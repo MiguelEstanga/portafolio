64867e838ae2e400dcc9962612e59121c0e2a230
MiguelEstanga <115328770+MiguelEstanga@users.noreply.github.com>
2022-10-17 17:09:58 -0400

    correcion de errorea

--- src/Componentes3/Sobremi/Sobremi.jsx
+++ src/Componentes3/Sobremi/Sobremi.jsx
@@ -17,16 +17,16 @@ export const Sobremi = () => {
           </h2>
         </Titulo>
         Mi nombre es <span>Miguel Alejandro Estanga Martinez</span> estudiante ing de Sistemas
-        en la univercidad de oriente udo vivo en Maturin Monagas, Me dedico a la programacion desde
-        los 17años empeze haciendo FrontEnd y luego algo  BackEnd actualmente tengo 24 año para la
-        fecha de  Obctubre del 2022 hago este portafolio con la intencion de dar un perfil como desarrollador 
-        y mostrar las tecnologia que domino actualmente.
-        Este sitio web esta diseñado con unas
-        de mis tecnologias favorita y actualmenta la que mas utilizo Reacjs en la seccion de Habilidades
-        muestro otras tecnologias que tambien utilizo muy amenudo <a href='#habilidades'>Habilidades</a>
-        Tambien pretendo utilizar este sitio como galeria para poder mostrar mis componentes , app y sitios web que estare haciendo mas
-        adelante los cuales podras ver en la sesion de <a href='#Proyectos'>Proyectos</a> actualmente son pocos
-        pero estare trabajando duro para subir mas contenido {":)"}
+        en la universidad de Oriente udo vivo en Maturin Monagas, Me dedico a la programación desde
+        los 17años empecé haciendo FrontEnd y luego algo  BackEnd actualmente tengo 24 años para la
+        fecha de  Obctubre del 2022 hago este portafolio con la intención de dar un perfil como desarrollador 
+        y mostrar las tecnología que domino actualmente.
+        Este sitio web está diseñado con unas
+        de mis tecnologias favorita y actualmente la que más utilizo Reacjs en la sección de Habilidades
+        muestro otras tecnologías que también utilizo muy a menudo <a href='#habilidades'>Habilidades</a>
+        También pretendo utilizar este sitio como galería para poder mostrar mis componentes , app y sitios web que estaré haciendo más
+        adelante los cuales podrás ver en la sección de <a href='#Proyectos'>Proyectos</a> actualmente son pocos
+        pero estaré trabajando duro para subir más contenido {":)"}
 
         <div className="datos">
           <div>
